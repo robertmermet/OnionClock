01a9353bca505841e8a1af4b8049e57e03d41d71
robert mermet <robertmermet@gmail.com>
2020-02-12 15:05:29 -0500

    added crescent moon to second hand

--- clock.js
+++ clock.js
@@ -223,7 +223,13 @@ window.addEventListener('load', function load() {
         drawHand(minute, -radius * .25, -radius * .48);
         // Draw second hand
         second = (second * Math.PI / 30) + (millisec * Math.PI / (30 * 1000));
-        drawHand(second, 0, -radius * .23);
+        drawHand(second, radius * .03, -radius * .23);
+        // Draw crescent moon
+        ctx.beginPath();
+        ctx.rotate(second);
+        ctx.arc(0, radius * .04, radius * .012, .7 * Math.PI, 2.3 * Math.PI);
+        ctx.stroke();
+        ctx.rotate(-second);
     }
 
     function drawHand(pos, moveTo, length) {
